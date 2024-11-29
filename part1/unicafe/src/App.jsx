b58faa273ea6b79ef6aac7d64be1bc46915f7915
changsyuanwu <william.wu1219@gmail.com>
2024-11-29 16:45:54 +0800

    e1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,15 +9,37 @@ const Header = ({ text }) => (
   <h1>{text}</h1>
 )
 
+const Statistics = ({ stats }) => {
+  return (
+    <>
+      <Header text="Statistics" />
+      <p>Good {stats.good}</p>
+      <p>Neutral {stats.neutral}</p>
+      <p>Bad {stats.bad}</p>
+      <p>All {stats.all}</p>
+      <p>Average {stats.average}</p>
+      <p>Positive {stats.positivePercent}%</p>
+    </>
+  );
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const all = good + neutral + bad;
-  const average = all > 0 ? good - bad / all : 0;
-  const positivePercent = all > 0 ? good / all * 100 : 0;
+  const all = good + neutral + bad
+
+  const stats = {};
+  stats.good = good;
+  stats.neutral = neutral;
+  stats.bad = bad;
+  stats.all = all;
+  stats.average = all > 0 ? good - bad / all : 0;
+  stats.positivePercent = all > 0 ? good / all * 100 : 0;
+
+  
 
   return (
     <div>
@@ -25,13 +47,7 @@ const App = () => {
       <Button text="Good" handleClick={() => setGood(good + 1)} />
       <Button text="Neutral" handleClick={() => setNeutral(neutral + 1)} />
       <Button text="Bad" handleClick={() => setBad(bad + 1)} />
-      <Header text="Statistics" />
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {all}</p>
-      <p>Average {average}</p>
-      <p>Positive {positivePercent}%</p>
+      <Statistics stats={stats} />
     </div>
   );
 };
