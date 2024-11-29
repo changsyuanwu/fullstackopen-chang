305f6dfa1548b490423441b1ffcb24835bd857c9
changsyuanwu <william.wu1219@gmail.com>
2024-11-29 16:17:11 +0800

    finished p1d lecture

--- part1/p1c/statedemo/src/App.jsx
+++ part1/p1c/statedemo/src/App.jsx
@@ -1,37 +1,23 @@
 import { useState } from "react";
-
-const Display = ({ counter }) => <div>{counter}</div>;
-
-const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>;
+import "./App.css";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [value, setValue] = useState(10);
 
-  console.log("rendering with counter value", counter);
-
-  const increaseByOne = () => {
-    console.log("increasing, value before", counter);
-    setCounter(counter + 1);
-  };
-
-  const decreaseByOne = () => {
-    console.log("decreasing, value before", counter);
-    setCounter(counter - 1);
-  };
-
-  const setToZero = () => {
-    console.log("resetting to zero, value before", counter);
-    setCounter(0);
+  const setToValue = (newValue) => () => {
+    console.log("value now", newValue); // print the new value to console
+    setValue(newValue);
   };
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      {value}
+
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   );
-}; 
+};
 
 export default App;
