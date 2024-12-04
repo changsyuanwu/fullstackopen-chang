a31b4a3aaaf31b8094a0c495b3078ef5c6880f09
changsyuanwu <william.wu1219@gmail.com>
2024-12-04 13:47:19 +0800

    yes semicolons

--- example/notes-backend/mongo.js
+++ example/notes-backend/mongo.js
@@ -1,29 +1,29 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", false)
+mongoose.set("strictQuery", false);
 
-const url = process.env.MONGODB_ATLAS_URL
-console.log("connecting to", url)
+const url = process.env.MONGODB_ATLAS_URL;
+console.log("connecting to", url);
 
 mongoose.connect(url)
   .then(res => {
-    console.log("connected to MongoDB")
+    console.log("connected to MongoDB");
   })
   .catch(err => {
-    console.log("error connecting to MongoDB", err.message)
-  })
+    console.log("error connecting to MongoDB", err.message);
+  });
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-})
+});
 
-const Note = mongoose.model("Note", noteSchema)
+const Note = mongoose.model("Note", noteSchema);
 
 const note = new Note({
   content: "HTML is easy",
   important: true,
-})
+});
 
 // note.save().then((result) => {
 //   console.log("note saved!");
@@ -33,7 +33,7 @@ const note = new Note({
 
 Note.find({}).then(res => {
   res.forEach(note => {
-    console.log(note)
-  })
-  mongoose.connection.close()
-})
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
