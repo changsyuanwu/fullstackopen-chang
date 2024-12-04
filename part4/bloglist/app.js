17f8e995ada5ab43c06a44fbe950743f988d5adb
changsyuanwu <william.wu1219@gmail.com>
2024-12-04 15:53:33 +0800

    refactor blogslist, add linting

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,38 +3,17 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const blogsRouter = require("./controllers/blogs");
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl)
   .then(() => {
-  console.log(`connected to ${mongoUrl}`)
-})
+    console.log(`connected to ${mongoUrl}`);
+  });
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-  console.log(blog)
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-module.exports = app
+module.exports = app;
