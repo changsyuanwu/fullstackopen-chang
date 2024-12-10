6e5caef7e7c57af2bb014530907187263ff36dea
changsyuanwu <william.wu1219@gmail.com>
2024-12-10 11:23:10 +0800

    e5.1, 5.2 add login to bloglist frontend

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,24 +1,84 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import LoginPage from './components/LoginPage'
+import loginService from './services/login'
+import Notification from './components/Notification'
+import "./App.css";
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+      setBlogs(blogs)
+    );
+  }, [user]);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBloglistAppUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+
+      window.localStorage.setItem("loggedBloglistAppUser", JSON.stringify(user));
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 3000);
+    }
+  };
+
+  const handleLogout = () => {
+    window.localStorage.removeItem("loggedBloglistAppUser");
+    setUser(null);
+  };
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      <Notification message={errorMessage} />
+      {user === null ? (
+        <LoginPage
+          username={username}
+          password={password}
+          handleLogin={handleLogin}
+          setUsername={setUsername}
+          setPassword={setPassword}
+        />
+      ) : (
+        <div>
+          <h1>Blogs</h1>
+          <span>
+            {user.name} logged in
+            <button onClick={handleLogout}>logout</button>
+          </span>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </div>
       )}
     </div>
-  )
+  );
 }
 
 export default App
---
+++ part5/bloglist-frontend/src/components/LoginPage.jsx
@@ -0,0 +1,18 @@
+import LoginForm from "./LoginForm"
+
+const LoginPage = ({ username, password, handleLogin, setUsername, setPassword }) => {
+  return (
+    <div>
+      <h1>Log in to Chang's bloglist app</h1>
+      <LoginForm
+        username={username}
+        password={password}
+        handleLogin={handleLogin}
+        setUsername={setUsername}
+        setPassword={setPassword}
+      />
+    </div>
+  );
+}
+
+export default LoginPage
