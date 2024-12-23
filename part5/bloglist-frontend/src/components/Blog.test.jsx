bfe42334e5346faf2c1bbd9a9ba2b645b8e66830
changsyuanwu <william.wu1219@gmail.com>
2024-12-23 17:00:00 +0800

    e5.13 test that blogs render title and author while not url and likes

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import Blog from "./Blog";
 import userEvent from "@testing-library/user-event";
-import { vi } from "vitest";
+import { expect, vi } from "vitest";
 
-test();
+test("renders title and author", () => {
+  const blog = {
+    title: "Testing Blogs",
+    author: "Testing Author",
+    url: "https://testing.com",
+    likes: 1,
+  };
+
+  const { container } = render(<Blog blog={blog} />);
+
+  const blogEl = container.querySelector(".blog");
+  expect(blogEl).toHaveTextContent(blog.title);
+  expect(blogEl).toHaveTextContent(blog.author);
+
+  const elementContainingURL = screen.queryByText(blog.url);
+  expect(elementContainingURL).toBeNull();
+  const elementContainingLikes = screen.queryByText(blog.likes.toString());
+  expect(elementContainingLikes).toBeNull();
+});
