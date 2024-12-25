5f83676d7dc907c84e92487624bbb3b9e660751a
changsyuanwu <william.wu1219@gmail.com>
2024-12-25 11:16:12 +0800

    use playwright base url and refactor note creation

--- example/playwright-e2e-tests/tests/note_app.spec.js
+++ example/playwright-e2e-tests/tests/note_app.spec.js
@@ -1,10 +1,10 @@
 const { test, expect, describe, beforeEach } = require("@playwright/test");
-const { loginWith } = require("./helper");
+const { loginWith, createNote } = require("./helper");
 
 describe('Note app', () => {
   beforeEach(async ({ page, request }) => {
-    await request.post("http://localhost:3002/api/testing/reset");
-    await request.post("http://localhost:3002/api/users", {
+    await request.post("/api/testing/reset");
+    await request.post("/api/users", {
       data: {
         name: "Test User",
         username: "testing",
@@ -12,7 +12,7 @@ describe('Note app', () => {
       },
     });
 
-    await page.goto("http://localhost:5173");
+    await page.goto("/");
   });
 
   test("front page can be opened", async ({ page }) => {
@@ -51,11 +51,7 @@ describe('Note app', () => {
     });
 
     test("a new note can be created", async ({ page }) => {
-      await page.getByRole("button", { name: "new note" }).click();
-      await page
-        .getByTestId("newNoteContent")
-        .fill("a note created by playwright");
-      await page.getByRole("button", { name: "save" }).click();
+      await createNote(page, "a note created by playwright");
       await expect(
         page.getByText("a note created by playwright")
       ).toBeVisible();
@@ -63,9 +59,7 @@ describe('Note app', () => {
 
     describe("and a note exists", () => {
       beforeEach(async ({ page }) => {
-        await page.getByRole("button", { name: "new note" }).click();
-        await page.getByRole("textbox").fill("another note by playwright");
-        await page.getByRole("button", { name: "save" }).click();
+        await createNote(page, "another note by playwright");
       });
 
       test("importance can be changed", async ({ page }) => {
