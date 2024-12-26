22bcd1d076489c8f1b66520de774b175b9217d0e
changsyuanwu <william.wu1219@gmail.com>
2024-12-26 11:58:18 +0800

    refactor redux notes to use action creators

--- part6/redux-example/src/main.jsx
+++ part6/redux-example/src/main.jsx
@@ -7,26 +7,35 @@ const store = createStore(noteReducer);
 
 const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
+// These functions create actions and are thus called action creators
+const createNote = (content) => {
+  return {
+    type: "NEW_NOTE",
+    payload: {
+      content,
+      important: false,
+      id: generateId(),
+    },
+  };
+};
+
+const toggleImportanceOf = (id) => {
+  return {
+    type: "TOGGLE_IMPORTANCE",
+    payload: { id },
+  };
+};
+
 const App = () => {
   const addNote = (event) => {
     event.preventDefault();
     const content = event.target.note.value;
     event.target.note.value = "";
-    store.dispatch({
-      type: "NEW_NOTE",
-      payload: {
-        content,
-        important: false,
-        id: generateId(),
-      },
-    });
+    store.dispatch(createNote(content));
   };
 
   const toggleImportance = (id) => {
-    store.dispatch({
-      type: "TOGGLE_IMPORTANCE",
-      payload: { id },
-    });
+    store.dispatch(toggleImportanceOf(id));
   };
 
   return (
