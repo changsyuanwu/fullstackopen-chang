3cd87a4c63c3159acffeed4bcd9f4f603769e10f
changsyuanwu <william.wu1219@gmail.com>
2024-12-26 13:48:18 +0800

    refactor redux notes to use provider and seperate logic into components

--- part6/redux-example/src/reducers/noteReducer.js
+++ part6/redux-example/src/reducers/noteReducer.js
@@ -16,4 +16,26 @@ const noteReducer = (state = [], action) => {
   }
 };
 
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+
+// These functions create actions and are thus called action creators
+export const createNote = (content) => {
+  return {
+    type: "NEW_NOTE",
+    payload: {
+      content,
+      important: false,
+      id: generateId(),
+    },
+  };
+};
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: "TOGGLE_IMPORTANCE",
+    payload: { id },
+  };
+};
+
 export default noteReducer;
