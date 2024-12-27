82a0c3b493928fdc594bc83e5fbae720eed9bc5d
changsyuanwu <william.wu1219@gmail.com>
2024-12-27 10:27:52 +0800

    refactor notes redux to use redux toolkit

--- part6/redux-notes/src/main.jsx
+++ part6/redux-notes/src/main.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import { createStore, combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 
 import filterReducer from "./reducers/filterReducer";
 import noteReducer from "./reducers/noteReducer";
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
+const store = configureStore({
+  reducer: {
+    filter: filterReducer,
+    notes: noteReducer,
+  },
 });
-
-const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
