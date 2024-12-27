8cc7f2b864fdeaf96d3d1de0e480de465a1b2814
changsyuanwu <william.wu1219@gmail.com>
2024-12-27 08:15:50 +0800

    added radio button filter for redux notes

--- part6/redux-notes/src/main.jsx
+++ part6/redux-notes/src/main.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createStore } from "redux";
-
 import { Provider } from "react-redux";
-
 import App from "./App";
-import noteReducer from "./reducers/noteReducer";
+import { createStore, combineReducers } from "redux";
 
-const store = createStore(noteReducer);
+import filterReducer from "./reducers/filterReducer";
+import noteReducer from "./reducers/noteReducer";
+import { createNote } from "./reducers/noteReducer";
+import { filterChange } from "./reducers/filterReducer";
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+});
+
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
