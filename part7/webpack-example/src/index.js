666accc8c6169795376ad8cc506b388758d6e1ee
changsyuanwu <william.wu1219@gmail.com>
2025-01-08 15:50:32 +0800

    add react and use babel loader to build webpack

--- part7/webpack-example/src/index.js
+++ part7/webpack-example/src/index.js
@@ -1,8 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const hello = (name) => {
-  console.log(`hello ${name}`);
-};
-
-App();
-hello("world");
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
