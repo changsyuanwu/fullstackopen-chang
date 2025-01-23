69630c8f7c97e860fb390d36007162394dc806dc
changsyuanwu <william.wu1219@gmail.com>
2025-01-23 16:42:16 +0800

    e8.3 allAuthors and add count of books published to author fields

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -102,6 +102,7 @@ const typeDefs = `
     name: String!
     id: ID!
     born: Int
+    bookCount: Int
   }
 
   type Book {
@@ -116,6 +117,7 @@ const typeDefs = `
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -124,7 +126,13 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
   },
+  Author: {
+    bookCount: (root) => {
+      return books.filter((b) => b.author === root.name).length;
+    },
+  }
 };
 
 const server = new ApolloServer({
