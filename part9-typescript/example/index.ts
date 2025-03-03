1852984c1a42ee4f964f436b0720616f93422276
changsyuanwu <william.wu1219@gmail.com>
2025-03-03 13:13:39 +0800

    add data validation for calculate endpoint

--- part9-typescript/example/index.ts
+++ part9-typescript/example/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-import { calculator } from "./calculator";
+import { calculator, Operation } from "./calculator";
 
 app.use(express.json());
 
@@ -9,9 +9,19 @@ app.get("/ping", (_req, res) => {
 });
 
 app.post("/calculate", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { value1, value2, op } = req.body;
 
-  const result = calculator(Number(value1), Number(value2), op);
+  if (!value1 || !value2 || !op) {
+    res.status(400).send({ error: "parameters missing" });
+  }
+
+  if (isNaN(Number(value1)) || isNaN(Number(value2))) {
+    res.status(400).send({ error: "malformatted parameters" });
+  }
+
+  // assert the type of op (basically casting)
+  const result = calculator(Number(value1), Number(value2), op as Operation);
   res.send({ result });
 });
 
