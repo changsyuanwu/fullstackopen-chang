34a20b720c774fbe6ddc6842c002162de3836072
changsyuanwu <william.wu1219@gmail.com>
2025-03-07 13:30:01 +0800

    e9.10 restructure patientor backend and add endpoint to fetch all diagnoses

--- part9-typescript/patientor/backend/src/index.ts
+++ part9-typescript/patientor/backend/src/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
+import diagnosesRouter from "./routes/diagnoses";
 
 const app = express();
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors());
 app.use(express.json());
 
@@ -13,6 +13,8 @@ app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
+
+app.use("/api/diagnoses", diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ part9-typescript/patientor/backend/src/routes/diagnoses.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { Response } from "express";
+import { Diagnosis } from "../types";
+import diagnosisService from "../services/diagnosisService";
+
+const router = express.Router();
+
+router.get("/", (_req, res: Response<Diagnosis[]>) => {
+  res.send(diagnosisService.getDiagnoses());
+});
+
+export default router;
