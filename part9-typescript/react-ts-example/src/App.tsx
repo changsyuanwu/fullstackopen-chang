b4bc597550ac706aaa9046a0980715124b88cd6e
changsyuanwu <william.wu1219@gmail.com>
2025-03-11 13:09:00 +0800

    add form for adding new notes

--- part9-typescript/react-ts-example/src/App.tsx
+++ part9-typescript/react-ts-example/src/App.tsx
@@ -8,8 +8,25 @@ const App = () => {
     { id: "1", content: "testing" }
   ]);
 
+  const noteCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const noteToAdd = {
+      content: newNote,
+      id: String(notes.length + 1),
+    };
+    setNotes(notes.concat(noteToAdd));
+    setNewNote("");
+  };
+
   return (
     <div>
+      <form onSubmit={noteCreation}>
+        <input
+          value={newNote}
+          onChange={(event) => setNewNote(event.target.value)}
+        />
+        <button type="submit">add</button>
+      </form>
       <ul>
         {notes.map((note) => (
           <li key={note.id}>{note.content}</li>
