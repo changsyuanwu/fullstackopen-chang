531bf00bd800022a44c119566a68981a57391ef5
changsyuanwu <william.wu1219@gmail.com>
2025-03-13 14:40:35 +0800

    e9.26.5 add health check entry data display

---
+++ part9-typescript/patientor/frontend/src/components/PatientInfoPage/HealthCheckEntry.tsx
@@ -0,0 +1,30 @@
+import { Box, Typography } from "@mui/material";
+import MonitorHeartIcon from "@mui/icons-material/MonitorHeart";
+import { HealthCheckRating, type HealthCheckEntry } from "../../types";
+import DiagnosesList from "./DiagnosesList";
+
+interface Props {
+  entry: HealthCheckEntry;
+}
+
+const HealthCheckEntry = ({ entry }: Props) => {
+
+  return (
+    <Box component="fieldset">
+      <Typography>
+        {entry.date} <MonitorHeartIcon fontSize="large" />
+      </Typography>
+      <Typography sx={{ fontStyle: "italic" }}>{entry.description}</Typography>
+      <DiagnosesList diagnosisCodes={entry.diagnosisCodes} />
+      <Typography>
+        Health check result:{" "}
+        <Box component="span" sx={{ fontStyle: "italic" }}>
+          {HealthCheckRating[entry.healthCheckRating]}
+        </Box>
+      </Typography>
+      <Typography>diagnosed by {entry.specialist}</Typography>
+    </Box>
+  );
+};
+
+export default HealthCheckEntry;
